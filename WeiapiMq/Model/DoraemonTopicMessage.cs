1d054e120856bd3c3d531908f7f55476882b0ac0
Cui, Fawei <fawei.cui@philips.com>
2026-02-06 18:12:21 +0800

    ADD SIGNALR AND code clean

--- WeiapiMq/Model/DoraemonTopicMessage.cs
+++ WeiapiMq/Model/DoraemonTopicMessage.cs
@@ -1,4 +1,6 @@
-﻿namespace service.messaging.Model
+﻿using service.messaging.Model;
+
+namespace service.messaging.Model
 {
-    public record DoraemonTopicMessage(string Topic, string ImagePath, string Message);
+    public record DoraemonTopicMessage(string Topic, string ImagePath, string Message) : ITopicMessage;
 }
